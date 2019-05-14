a9f58ec9a5d70ce8641e2ad38a724a08e4267b0c
Hari <harihara.earlapati@gmail.com>
2019-05-13 23:18:44 -0500

    Able to fetch currency value and send email

--- src/main/java/com/hari/currencyconverter/email/EmailServiceImpl.java
+++ src/main/java/com/hari/currencyconverter/email/EmailServiceImpl.java
@@ -1,12 +1,48 @@
 package com.hari.currencyconverter.email;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
+    private static final Logger LOGGER = LogManager.getLogger(EmailServiceImpl.class);
+
+    private final JavaMailSender javaMailSender;
+
+    private final String toEmail;
+
+    private final String fromEmail;
+
+    @Autowired
+    public EmailServiceImpl(JavaMailSender javaMailSender,
+                            @Value("${email.to}") String toEmail,
+                            @Value("${spring.mail.username}") String fromEmail) {
+        this.javaMailSender = javaMailSender;
+        this.toEmail = toEmail;
+        this.fromEmail = fromEmail;
+    }
+
     @Override
     public void sendEmail(String body) {
+
+        try {
+            final SimpleMailMessage email = new SimpleMailMessage();
+            email.setSubject("Currency Conversion Rate");
+            email.setTo(toEmail);
+            email.setText(body);
+            email.setFrom(fromEmail);
+
+            javaMailSender.send(email);
+        }
+        catch (Exception e) {
+            LOGGER.error("Exception when sending email " , e);
+        }
 
     }
 }
---
+++ src/main/java/com/hari/currencyconverter/util/EmailConfig.java
@@ -0,0 +1,18 @@
+package com.hari.currencyconverter.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+//@Configuration
+public class EmailConfig {
+
+    /*public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender
+
+
+    }*/
+
+}
